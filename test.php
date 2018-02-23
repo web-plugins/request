7004368150246d24cbfccc0bc6325767c1a07d64
tangxiangmin <645234650@qq.com>
2018-02-23 16:07:46 +0800

    init

---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+//$data = [
+//    "id" => 1,
+//    "price" => 100,
+//    "name" => "gooddddd"
+//];
+//exit(json_encode($data));
+
+// 异步传图片
+//$imgData = $_REQUEST['file'];
+//
+//$base64 = explode(',', $imgData)[1];
+//$img = base64_decode($base64);
+//
+//$url = 'test.jpg';
+//if (file_put_contents($url, $img)) {
+//    $res = array(
+//        "url" => $url
+//    );
+//}else {
+//    $res = array(
+//        "msg" => "error"
+//    );
+//}
+//exit(json_encode($res));
+
